f4ba0181363a3afef2d24befc4bf9055b5927284
Nipuna Dodantenna <nipunadodan@gmail.com>
2022-05-26 15:43:02 +0530

    added Theme toggle to home page and styled the home pag

--- src/components/common/ThemeToggle.js
+++ src/components/common/ThemeToggle.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { ThemeContext } from './ThemeContext';
 
-const ToggleToggle = () => {
+const ThemeToggle = (props) => {
     const { theme, setTheme } = React.useContext(ThemeContext);
 
     return (
-        <div className="transition duration-500 ease-in-out rounded-full p-2">
+        <div className={"inline-block transition duration-500 ease-in-out rounded-full py-1 px-6 bg-gray-100 dark:bg-gray-700 cursor-pointer text-gray-500 dark:text-gray-400 text-xl "+ props.className} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
             {theme === 'dark' ? (
-                <i
-                    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                    className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer la la-moon"
-                />
+                <i className="la la-moon" />
             ) : (
-                <i
-                    onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                    className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer la la-sun"
-                />
+                <i className="la la-sun" />
             )}
         </div>
     );
 };
 
-export default ToggleToggle;
+export default ThemeToggle;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import ThemeToggle from "../common/ThemeToggle";
 
 const Home = () => {
     return (
-        <>Home</>
+        <div className={'dark:bg-gray-800 dark:text-white text-dark min-h-screen flex items-center justify-center transition-all'}>
+            <div className={'text-center'}>
+                <h1 className={'text-6xl mb-3'}>Test</h1>
+                <hr className={'w-80 max-w-full'} />
+                <h2 className={'text-xl mt-3'}>React.js Boilerplate</h2>
+                <ThemeToggle className={'mt-6'} />
+            </div>
+        </div>
     )
 }
 
